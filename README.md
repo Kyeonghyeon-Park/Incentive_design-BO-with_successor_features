35e4f31f263f2984af106e6bd9d980cf0fe105cd
David <60404601+davidmccl@users.noreply.github.com>
2021-02-22 18:48:22 -0500

    issues with over_all feature sending none values

--- README.md
+++ README.md
@@ -6,4 +6,18 @@ bayesian_optimization.py is used for upper level using alpha and f(alpha)
 
 gird_world.py defines the Shou and Di's taxi driver repositioning game (grid, state transition, ...)
 
-actor_critic.py contains the mean field actor-critic algorithm and several utils (draw plot, print statistics, ...)
+actor_critic.py contains the mean field actor-critic algorithm
+
+utils.py contains several utils (draw plot, print statistics, ...)
+
+
+## Update log summary
+210217 
+
+Extract some utils of `actor_critic.py` to clean the code (`utils.py`)
+
+Add various explanations in `grid_world.py`, `actor_critic.py`, `bayesian_optimization.py`, `utils.py` and `main.py`
+
+Add some to-do lists in `bayesian_optimization.py` and `actor_critic.py`
+
+Modify the step function to return the figure in `grid_world.py`
--- bayesian_optimization.py
+++ bayesian_optimization.py
@@ -16,7 +16,28 @@ from actor_critic import ActorCritic
 
 
 #%%
+"""
+To-do or check list
+--------
+Current package is not stable
+Current BO setting is not same as the Shou and Di's paper (I almost use the initial setting of the package) 
+"""
+
 def get_args(alpha):
+    """
+    Build args which is the parameter setting of the actor-critic network
+
+    Parameters
+    ----------
+    alpha : float
+        Designer's decision (penalty for overcrowded grid)
+        BO will decide the proper alpha
+
+    Returns
+    -------
+    args : argparse.Namespace
+        Return the args
+    """
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--designer_alpha', default=alpha)
@@ -40,10 +61,27 @@ def get_args(alpha):
 
 
 def black_box_function(alpha):
+    """
+    Define the black box function which is the f(alpha)
+    We have to get the designer's objective after the algorithm converges
+
+    Parameters
+    ----------
+    alpha : float
+        Designer's decision (penalty for overcrowded grid)
+        BO will decide the proper alpha
+
+    Returns
+    obj_for_BO : float
+        Return the designer's objective for BO which is the average of the obj_ftn
+    -------
+
+    """
     args = get_args(alpha)
     model = ActorCritic(args)
     model.run()
-    return np.average(model.outcome['test']['obj_ftn'][-100:])
+    obj_for_BO = np.average(model.outcome['test']['obj_ftn'][-100:])
+    return obj_for_BO
 
 
 #%%
