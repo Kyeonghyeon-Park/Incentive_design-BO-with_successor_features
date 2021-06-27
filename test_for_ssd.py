e0011cfcbabb27befad565c1564fdebd3cd828f5
Kyeonghyeon-Park <guspk0212@gmail.com>
2021-06-27 20:49:46 +0900

    Main file with building networks

--- test_for_ssd.py
+++ test_for_ssd.py
@@ -7,8 +7,8 @@ import shutil
 import numpy as np
 
 # import pathmagic
-
-from config.default_args import add_default_args
+from parsed_args import args
+from networks_ssd import Networks
 from social_dilemmas.envs.env_creator import get_env_creator
 import utility_funcs
 
@@ -19,56 +19,54 @@ Notes
     If you can't, try "import pathmagic"    
 '''
 
-# TODO 1. Build Actor network, critic network, psi network
-# TODO 2. Action selection based on actor network
+# TODO 1. Build {Actor network, critic network, psi network} or {critic network, psi network}
+# TODO 2. Action selection based on {actor network} or {critic network, psi network}
 # TODO 3. Update network
 
-# Initial setting
-parser = argparse.ArgumentParser()
-add_default_args(parser)
-
-# Our initial setting
-parser.add_argument("--lv_penalty", type=float, default=0, help="Penalty level for agents who eat apple")
-parser.add_argument("--lv_incentive", type=float, default=0, help="Incentive level for agents who clean the river")
-parsed_args = parser.parse_args()
-
-# Our setting
-parsed_args.env = 'cleanup_modified'
-parsed_args.num_agents = 3  # Maximum 10 agents
-parsed_args.lv_penalty = 0.5
-parsed_args.lv_incentive = 0.3
+# TODO : trained 추가?
 
 random.seed(1234)
 np.random.seed(1234)
-horizon = 200  # Maximum period for the run
+horizon = args.horizon
 
 # Build environment
-env_creator = get_env_creator(parsed_args.env, parsed_args.num_agents, parsed_args)
-env = env_creator(parsed_args.num_agents)
-observations = env.reset()
+env_creator = get_env_creator(args.env, args.num_agents, args)
+env = env_creator(args.num_agents)
+_ = env.reset()
 
+# Build networks
+networks = Networks(env, args)
+
+# exploration prob.
+epsilon = 0
+
+# Build path for saving images
 path = "saved_video"
 image_path = os.path.join(path, "frames/")
 if not os.path.exists(image_path):
     os.makedirs(image_path)
-shape = env.world_map.shape
-full_obs = [np.zeros((shape[0], shape[1], 3), dtype=np.uint8) for i in range(horizon)]
+
+# Not needed right now
+# shape = env.world_map.shape
+# full_obs = [np.zeros((shape[0], shape[1], 3), dtype=np.uint8) for i in range(horizon)]
 
 buffers = []
 
 for i in range(horizon):
     agents = list(env.agents.values())
-    action_dim = env.action_space.n
     agents_actions_dict = dict()
     for agent in agents:
         # Action selection for agents
-        #############
-        # TODO 2. Action selection based on actor network based on current observations
-        rand_action = np.random.randint(action_dim)
-        #############
-        agents_actions_dict[agent.agent_id] = rand_action
+        exploration = np.random.rand(1)[0]
+        if exploration < epsilon:
+            action = np.random.randint(networks.action_dim)
+        else:
+            observation = env.single_map_to_idx(env.symbol_view_ind(agent))
+            action = networks.get_action(observation, is_target=False)
+        agents_actions_dict[agent.agent_id] = action
 
-    obs, rew, dons, info, fea, exp = env.step(agents_actions_dict)
+    obs, rew, dons, info, fea, exp_color, exp_idx = env.step(agents_actions_dict)
+    # TODO. 이 reward를 더해서 cumulative reward로 만들기
 
     sys.stdout.flush()
 
@@ -77,11 +75,12 @@ for i in range(horizon):
         env.render(filename=image_path + "frame" + str(i).zfill(6) + ".png")
         if i % 10 == 0:
             print("Saved frame " + str(i) + "/" + str(horizon))
-    rgb_arr = env.full_map_to_colors()
-    full_obs[i] = rgb_arr.astype(np.uint8)
+
+    # rgb_arr = env.full_map_to_colors()
+    # full_obs[i] = rgb_arr.astype(np.uint8)
 
     # Add samples to buffer
-    buffers.append(exp)
+    buffers.append(exp_idx)
 
     # TODO 3. Update network
 
@@ -92,8 +91,8 @@ if path is None:
     if not os.path.exists(path):
         os.makedirs(path)
 
-fps = 5
-video_name = parsed_args.env + "_trajectory"
+fps = 3
+video_name = args.env + "_trajectory"
 utility_funcs.make_video_from_image_dir(path, image_path, fps=fps, video_name=video_name)
 
 # Clean up images
