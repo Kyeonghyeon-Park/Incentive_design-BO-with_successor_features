755fa1c9a633a56161f6b41987bf90c4c5c32aac
Kyeonghyeon-Park <guspk0212@gmail.com>
2022-12-12 14:27:43 +0900

    Add plot function for the lower-level comparison.

---
+++ plot/plot_ssd_lower_level_comparsion.py
@@ -0,0 +1,36 @@
+import numpy as np
+import torch
+
+from utils import utils_ssd
+
+
+''' 
+Code for getting the lower-level transfer figure. 
+'''
+# Transfer.
+dict_l_list = ["../results/211008 submitted version/results_ssd_final/alpha=0.33 using alpha=0.50 (2 seeds)/seed 1278 (original)/outcomes.tar",
+               "../results/211008 submitted version/results_ssd_final/alpha=0.33 using alpha=0.50 (2 seeds)/seed 1279/outcomes.tar",
+               ]
+# Non-transfer.
+dict_r_list = ["../results/211008 submitted version/results_ssd_final/alpha=0.33 (5 seeds)/seed 1267 (original)/outcomes.tar",
+               "../results/211008 submitted version/results_ssd_final/alpha=0.33 (5 seeds)/seed 1269/outcomes.tar",
+               "../results/211008 submitted version/results_ssd_final/alpha=0.33 (5 seeds)/seed 1270/outcomes.tar"
+               ]
+# 31 or 30.
+num_networks = 30
+outcomes_l = np.empty([num_networks, 0])
+outcomes_r = np.empty([num_networks, 0])
+
+# Collect and concatenate outcomes of the transfer results.
+for l in dict_l_list:
+    dict_l = torch.load(l)
+    outcomes_l_one_seed = dict_l["obj_full"]
+    outcomes_l = np.concatenate((outcomes_l, outcomes_l_one_seed), axis=1)
+# Collect and concatenate outcomes of the non-transfer results.
+for r in dict_r_list:
+    dict_r = torch.load(r)
+    outcomes_r_one_seed = dict_r["obj_full"]
+    outcomes_r = np.concatenate((outcomes_r, outcomes_r_one_seed), axis=1)
+
+
+utils_ssd.get_plt_final_aggregate_grayscale(outcomes_l, outcomes_r, is_3000=False)
